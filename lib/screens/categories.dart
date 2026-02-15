4f614d25437cea28f3e41c23e79f2f967c2fc297
mohammedalqirsh-stack <mohammedalqirsh@gmail.com>
2026-02-15 11:11:56 +0200

    add new file

---
+++ lib/screens/categories.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+ 
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+
+  void _selectCategory (BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title:'Some title',
+         meals: [], 
+         ),
+    ),
+    ); 
+  }
+   
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pick your category')),
+      body: GridView(
+        padding: EdgeInsets.all(24),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children:  [
+
+         for (final category in availableCategories)
+         CategoryGridItem(category: category, onSelectCategory: (){
+          _selectCategory(context);
+         } ,)
+        ],
+      ),
+    );
+  }
+}
