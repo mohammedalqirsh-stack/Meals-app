b322739d78fd9da053f75e492467e0fc5b5e352f
mohammedalqirsh-stack <mohammedalqirsh@gmail.com>
2026-02-25 14:40:34 +0200

    new adding

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -4,20 +4,23 @@ import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key,this.title, required this.meals});
+  const MealsScreen({super.key,this.title, required this.meals,required this.onToggleFavorite});
 
   final String? title;
   final List<Meal> meals;
+    final void Function (Meal meal) onToggleFavorite;
+
  
   void selectMeal(BuildContext context, Meal meal) {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealDetailsScreen(
           meal: meal, 
+          onToggleFavorite: onToggleFavorite,
          ),
        ),
      );
-  }  
+  } 
   @override 
   Widget build(BuildContext context) {
     Widget content = Center(
@@ -25,7 +28,7 @@ class MealsScreen extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           Text(
-            'Uo ho ... nothing here',
+            'Uo ho ... nothing here', 
             style: Theme.of(context).textTheme.headlineLarge!.copyWith(
               color: Theme.of(context).colorScheme.onBackground,
             ),
