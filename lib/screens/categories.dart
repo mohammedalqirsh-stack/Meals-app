e4a0ba64940bae52b13d0f3ad5feae0451561b2e
mohammedalqirsh-stack <mohammedalqirsh@gmail.com>
2026-03-12 00:39:00 +0200

    adding

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -7,9 +7,10 @@ import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key,required this.onToggleFavorite});
+  const CategoriesScreen({super.key,required this.onToggleFavorite,required this.availableMeals});
 
  final void Function (Meal meal) onToggleFavorite;
+ final List <Meal> availableMeals;
 
 
 
