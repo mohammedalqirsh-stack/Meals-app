3eef8b9f7f7de592097daa3a37136e5ec9644b19
mohammedalqirsh <mohammedalqirsh@gmail.com>
2026-06-15 13:35:06 +0300

    Update

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:meals/providers/meals_provider.dart';
 
 
 enum Filter {
@@ -30,4 +32,34 @@ class FiltersNotifier extends StateNotifier <Map<Filter, bool>>{
 
 final filtersProvider = 
 StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
-  (ref) =>FiltersNotifier());
+  (ref) =>FiltersNotifier(),
+);
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filtersProvider);
+  return meals.where((meal) {
+
+      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+      return true;
+    }).toList();
+});
+
+  
+  
+
+
+final filtersStateProvider = Provider((ref){
+
+});
