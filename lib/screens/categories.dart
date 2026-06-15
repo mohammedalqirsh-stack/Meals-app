6c407d3b78038feac256fa02845cd825ece47f20
mohammedalqirsh <mohammedalqirsh@gmail.com>
2026-06-15 14:33:22 +0300

    AnimationController

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,13 +6,36 @@ import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key,required this.availableMeals});
 
  
  final List <Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
 
+class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+  } 
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   void _selectCategory (BuildContext context, Category category) {
   final filteredMeals = dummyMeals
@@ -30,8 +53,6 @@ class CategoriesScreen extends StatelessWidget {
     );  
   } 
 
-     
-   
   @override
   Widget build(BuildContext context) {
     return 
