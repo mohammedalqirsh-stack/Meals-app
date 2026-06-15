0dce0de78ac02602eaabfe84563b26cfd63941c3
mohammedalqirsh <mohammedalqirsh@gmail.com>
2026-06-15 15:33:22 +0300

    animation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -29,6 +29,9 @@ class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerPr
       lowerBound: 0,
       upperBound: 1,
     );
+
+
+    _animationController.forward();
   } 
 
   @override
@@ -55,26 +58,32 @@ class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerPr
 
   @override
   Widget build(BuildContext context) {
-    return 
-       GridView( 
-        padding: EdgeInsets.all(24),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
+          mainAxisSpacing: 24,
+          crossAxisSpacing: 24,
           childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20, 
         ),
-        children:  [
-        
-         for (final category in availableCategories)
-         CategoryGridItem(
-          category: category, 
-           onSelectCategory: (){
-          _selectCategory(context, category);
-         } ,
-         )
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
+      ),
+       builder: (context,child) => Padding (padding : EdgeInsets.only(
+        top: 100- _animationController.value * 100,
+       ),
+       child: child),
+       );
       
-    );
+       
   }
 }
